73baf46ba3578f65aa77e5add15d16ecd602c3ea
fdemir <frndemir@gmail.com>
2024-02-22 08:17:55 +0300

    feat: database configuration and ignore files

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,6 @@
 import { drizzle } from 'drizzle-orm/libsql';
 import { createClient } from '@libsql/client';
 
-const client = createClient({ url: 'DATABASE_URL', authToken: 'DATABASE_AUTH_TOKEN' });
+const client = createClient({ url: process.env.DATABASE_URL!, authToken: process.env.DATABASE_AUTH_TOKEN });
 
 export const db = drizzle(client);
