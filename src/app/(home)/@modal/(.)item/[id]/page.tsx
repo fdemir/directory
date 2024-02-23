822d7972113614cdbbf641b6d187917791cd51b3
fdemir <frndemir@gmail.com>
2024-02-24 02:58:34 +0300

    feat: update item detail

--- src/app/(home)/@modal/(.)item/[id]/page.tsx
+++ src/app/(home)/@modal/(.)item/[id]/page.tsx
@@ -1,14 +1,15 @@
 import Item from "@/app/(home)/_components/item";
 import { getItemById, getItemsBySlug } from "@/app/(home)/actions";
+import { Badge } from "@/components/ui/badge";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import {
   Carousel,
   CarouselContent,
   CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
 } from "@/components/ui/carousel";
+import Link from "next/link";
 import { notFound } from "next/navigation";
+import { MdLink } from "react-icons/md";
 
 export default async function ItemDetail({
   params,
@@ -17,7 +18,7 @@ export default async function ItemDetail({
     id: string;
   };
 }) {
-  const item = await getItemById(params.id);
+  const item = await getItemById(Number(params.id));
 
   if (!item) {
     notFound();
@@ -29,10 +30,10 @@ export default async function ItemDetail({
         <Item data={item} />
       </CardHeader>
       <CardContent className="flex flex-col gap-4">
-        <Carousel className="w-full max-w-xs">
+        <Carousel className="w-full">
           <CarouselContent>
             {Array.from({ length: 5 }).map((_, index) => (
-              <CarouselItem key={index}>
+              <CarouselItem key={index} className="basis-1/3">
                 <div className="p-1">
                   <Card>
                     <CardContent className="flex aspect-square items-center justify-center p-6">
@@ -45,9 +46,19 @@ export default async function ItemDetail({
               </CarouselItem>
             ))}
           </CarouselContent>
-          <CarouselPrevious />
-          <CarouselNext />
         </Carousel>
+
+        <div className="my-3 flex gap-4">
+          <Link target="_blank" href="https://watchandchill.in/">
+            <Badge
+              variant="secondary"
+              className="px-3 py-2 text-md items-center flex gap-2 flex-shrink-0"
+            >
+              <span>App Store</span>
+              <MdLink className="w-6 h-6" />
+            </Badge>
+          </Link>
+        </div>
 
         <p>{item.description}</p>
       </CardContent>
