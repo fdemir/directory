922dd89da1c9f4045fdf143be29e92c3046e5b36
fdemir <frndemir@gmail.com>
2024-02-28 02:33:59 +0300

    fix: handle no item

--- src/app/(home)/_components/item-list.tsx
+++ src/app/(home)/_components/item-list.tsx
@@ -5,5 +5,12 @@ export default async function ItemList({
 }: {
   children: React.ReactElement[];
 }) {
+  if (children.length === 0) {
+    return (
+      <span className="text-muted-foreground">
+        There is no item in here. Submit one and it will show up here.
+      </span>
+    );
+  }
   return <div className="flex flex-col">{children}</div>;
 }
