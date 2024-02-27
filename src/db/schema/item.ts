1be694532f2aea8a983c103e284695deb87bdf39
fdemir <frndemir@gmail.com>
2024-02-28 02:34:10 +0300

    fix: update item schema

--- src/db/schema/item.ts
+++ src/db/schema/item.ts
@@ -6,12 +6,15 @@ import { type InferSelectModel } from "drizzle-orm";
 export const item = sqliteTable("item", {
   id: integer("id").primaryKey(),
   name: text("name").notNull(),
-  short_desc: text("short_name").notNull(),
-  description: text("desc").notNull(),
-  slug: text("slug").notNull(),
+  short_desc: text("short_desc").notNull(),
+  description: text("description").notNull(),
   created_at: text("timestamp").default(sql`CURRENT_TIMESTAMP`),
-  category_id: integer("category_id").references(() => category.id),
+  category_id: integer("category_id")
+    .references(() => category.id)
+    .default(0),
   url: text("url").notNull(),
+  // 0 = not verified, 1 = verified
+  verified: integer("verified").default(0),
 });
 
 export type Item = InferSelectModel<typeof item>;
